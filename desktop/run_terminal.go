aaacadc85574a68cbf35f8fb51612c398bb7d5b7
Xu Fasheng <fasheng.xu@gmail.com>
2016-02-16 12:17:02 +0800

    add copyright for source files
    Change-Id: I3b1ff95124af449c268a60888b8c536ba2af6c2f

--- desktop/run_terminal.go
+++ desktop/run_terminal.go
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) 2015 Deepin Technology Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ **/
+
 package desktop
 
 // #cgo pkg-config: gio-unix-2.0 gdk-3.0
